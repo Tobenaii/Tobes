d8e7b36e118d02a38209645a01ccbb24c860828c
Tobenai <akuapengin42@gmail.com>
2019-03-18 18:47:32 +1100

    Added 3D character model to test project

--- TestGame/src/TestApp.cpp
+++ TestGame/src/TestApp.cpp
@@ -1,6 +1,6 @@
-#include "..\..\Tobes\src\Tobes\Application.h"
 #include "TestApp.h"
 #include <iostream>
+#include <glm/common.hpp>
 
 Tobes::Application* Tobes::CreateApplication()
 {
@@ -15,7 +15,41 @@ TestApp::~TestApp()
 {
 }
 
+void TestApp::Startup()
+{
+	Shader* shader = new Shader();
+
+	shader->LoadVertexShader("H:\\Dev\\Projects\\Tobes\\Tobes\\TestGame\\content\\shaders\\vertex.txt");
+	shader->LoadFragmentShader("H:\\Dev\\Projects\\Tobes\\Tobes\\TestGame\\content\\shaders\\frag.txt");
+	shader->LinkProgram();
+	obj = new GameObject(glm::vec3(0, 0, 0));
+
+	obj->SetShader(shader);
+
+	vertices = new Vertex[4]{
+		Vertex(glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)),
+		Vertex(glm::vec3(0.5f, -0.5f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)),
+		Vertex(glm::vec3(0.5f, 0.5f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)),
+		Vertex(glm::vec3(-0.5f, 0.5f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)),
+	};
+
+	indices = new unsigned int[6]{
+		0,1,2,
+		2,3,0
+	};
+
+	Mesh* mesh = new Mesh("H:\\Dev\\Projects\\Tobes\\Tobes\\ContentPipeline\\Content\\advancedCharacter.tbs");
+	obj->SetMesh(mesh);
+}
+
 void TestApp::Update(float dt)
 {
-	std::cout << dt << std::endl;
+	//m_camera->SetPosition(m_camera->GetPosition() + glm::vec3(-1,0,0) * dt);
 }
+
+void TestApp::Draw()
+{
+	obj->Draw(m_renderer, m_camera);
+}
+
+
--- TestGame/src/TestApp.h
+++ TestGame/src/TestApp.h
@@ -7,5 +7,10 @@ public:
 	TestApp();
 	~TestApp();
 	virtual void Update(float dt);
+	virtual void Draw();
+	virtual void Startup();
+	GameObject* obj;
+	Vertex* vertices;
+	unsigned int* indices;
 };
 
